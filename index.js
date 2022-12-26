709911e31d9fce7ab4660a4daf73ed03ce7f25a5
murphyiank <109882059+murphyiank@users.noreply.github.com>
2022-12-26 11:53:14 -0700

    commit tree f9b519785942b1f12aed909c2a8d5b8d82916e7c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+        cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+        cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+        cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+        cats.shift();
+}
+
+function appendCat(){
+        const copyOfCats = [...cats, "Broom"];
+        return copyOfCats;
+}
+
+function prependCat(){
+        const copyOfCats = ["Arnold", ...cats];
+        return copyOfCats;
+}
+
+function removeLastCat(){
+       const newCats = cats.slice(0, -1);
+       return newCats;
+}
+
+function removeFirstCat(){
+        const newCats = cats.slice(1);
+        return newCats;
+}
